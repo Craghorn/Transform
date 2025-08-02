f903b838587ef43938f3a04445942bab3c8e9df1
Craghorn <Craghorn@gmail.com>
2025-08-02 09:32:46 +0300

    1

--- Assets/Scripts/CubeSecond.cs
+++ Assets/Scripts/CubeSecond.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class CubeSecond : MonoBehaviour
 {
     private float _moveSpeed = 1f;
-    private Vector3 _viewTo = new Vector3(1, 0, 1);
     private float _rotateSpeed = 10f;
-    private float _growSpeed = 0.001f;    
+    private float _growSpeed = 0.0001f;    
 
     private void Update()
     {
-        transform.Translate(_viewTo * _moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime);
         transform.localScale += new Vector3(_growSpeed, _growSpeed, _growSpeed);
     }
